c3667876777f9a6e1037104c2a91414d199eb352
Pim van den Broek <pim.vandenbroek@hotmail.com>
2024-12-02 07:43:48 +0100

    Created 2024 setup

---
+++ 2023/day-10/main.py
@@ -0,0 +1,14 @@
+lines = open("input.txt").readlines()
+
+firstamount = 0
+secondamount = 0
+
+###
+
+###
+print(f'First answer: {str(firstamount)}')
+
+###
+
+###
+print(f'Second answer: {str(secondamount)}')
