adb4a08dda8e1f9c758324afa0d6099219c825cd
Pim van den Broek <pim.vandenbroek@hotmail.com>
2024-12-04 15:45:20 +0100

    Added solution for Day 04

--- 2024/day-04/main.py
+++ 2024/day-04/main.py
@@ -1,29 +1,36 @@
-import time
 import os
+from helpers import tsprint
 
-data = open("test_input.txt").read().splitlines()
-data = [[y for y in x] for x in data]
-
-### Function to print timestamped messages
-startingtime = time.time()
-def tsprint(message):
-    timestamp = time.time()-startingtime
-    print(f"[{timestamp:.4f}] {message}")
+data = open(0).read().splitlines()
 
 ### Solution for part one
 def part_one():
   answer = 0
   for rownum,row in enumerate(data):
     for charnum,char in enumerate(row):
-      if char == 'X':
-
-        print(f"HIER STAAT EEN X, op regel {rownum} en kolom {charnum}")
+      if data[rownum][charnum] != 'X': continue
+      for directionrow in [-1,0,1]:
+        for directioncol in [-1,0,1]:
+          if directionrow == directioncol == 0: continue # Skip the current position
+          if not (0 <= rownum + 3 * directionrow < len(data) and 0 <= charnum + 3 * directioncol < len(data[0])): continue # Skip if out of bounds
+          if data[rownum+directionrow][charnum+directioncol] == 'M' and data[rownum+2*directionrow][charnum+2*directioncol] == 'A' and data[rownum+3*directionrow][charnum+3*directioncol] == 'S':
+            answer += 1
   tsprint(f'First answer: {str(answer)}')
 
 
 ### Solution for part two
 def part_two():
   answer = 0
+  for rownum,row in enumerate(data):
+    if rownum == 0 or rownum == len(data) - 1:
+        continue
+    for charnum,char in enumerate(row):
+      if charnum == 0 or charnum == len(data[0]) - 1:
+        continue
+      if data[rownum][charnum] != 'A': continue
+      dingskes = data[rownum-1][charnum-1] + data[rownum-1][charnum+1] + data[rownum+1][charnum+1] + data[rownum+1][charnum-1]
+      if dingskes in ["MMSS", "SSMM", "MSSM", "SMMS"]:
+        answer += 1
 
   tsprint(f'Second answer: {str(answer)}')
 
